f36bc56411a0cfe808f63c86656eba9069d8f25f
Dongwan Lee <dongwan_lee@naver.com>
2017-10-09 12:14:06 +0900

    Fixed Speed and Screen Issue

--- FullscreenWebView/src/main/java/com/example/khj/fullscreen/FullscreenActivity.java
+++ FullscreenWebView/src/main/java/com/example/khj/fullscreen/FullscreenActivity.java
@@ -2,6 +2,7 @@ package com.example.khj.fullscreen;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -38,7 +39,7 @@ import java.util.Map;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class FullscreenActivity extends Activity {
 
     String URL = "content://com.example.android.softkeyboard/keys";
     /**
@@ -60,57 +61,10 @@ public class FullscreenActivity extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
     private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
+    private boolean mVisible;
     private int interval = 1000; // Repeat this task every 5 seconds.
     private Handler handler;
 
@@ -133,9 +87,11 @@ public class FullscreenActivity extends AppCompatActivity {
         cr = getContentResolver();
         handler = new Handler();
 
+
         view.getSettings().setJavaScriptEnabled(true);
         view.enableSlowWholeDocumentDraw();
         view.setWebViewClient(new WebViewClient() {
+
             @Override
             public void onPageFinished(final WebView view, String url) {
                 updateKeyColor();
@@ -145,110 +101,28 @@ public class FullscreenActivity extends AppCompatActivity {
 
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-//        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-//    private static Bitmap pictureDrawable2Bitmap(PictureDrawable pictureDrawable) {
-//        Bitmap bitmap = Bitmap.createBitmap(pictureDrawable.getIntrinsicWidth()
-//                , pictureDrawable.getIntrinsicHeight()
-//                , Bitmap.Config.ARGB_8888);
-//        Canvas canvas = new Canvas(bitmap);
-//        canvas.drawPicture(pictureDrawable.getPicture());
-//        return bitmap;
-//    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        //mControlsView = findViewById(R.id.fullscreen_content_controls);
+        //mContentView = findViewById(R.id.fullscreen_content);
     }
 
     /* Start capturing web view repeatedly */
-
-
     Runnable updateKeyColors = new Runnable() {
         @Override
         public void run() {
-            view.measure(View.MeasureSpec.makeMeasureSpec(
+            int width, heigth;
+            width = view.getWidth(); heigth = view.getHeight();
+/*            view.measure(View.MeasureSpec.makeMeasureSpec(
                     View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
                     View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
             view.layout(0, 0, view.getMeasuredWidth(),
                     view.getMeasuredHeight());
-
+*/
             view.setDrawingCacheEnabled(true);
             view.buildDrawingCache();
-            Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(),
-                    view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-
-//            Canvas canvas = new Canvas(bitmap);
-//            Paint paint = new Paint();
-//            int height = bitmap.getHeight();
-//            canvas.drawBitmap(bitmap, 0, height, paint);
-//            view.draw(canvas);
-
+/*            Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(),
+                    view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);*/
+            Bitmap bitmap = Bitmap.createBitmap(width,
+                    heigth, Bitmap.Config.ARGB_8888);
             Cursor cursor = cr.query(Uri.parse(URL), null, null, null, null);// select
 
             String where = new String("x =? AND y=?");
@@ -266,6 +140,7 @@ public class FullscreenActivity extends AppCompatActivity {
                         cr.update(Uri.parse(URL), values1, "x="+x+" AND y="+y, null);
                     }
 //                    db.update("keys", values1, "x=" + x + " AND " + "y=" + y, null);
+
                 }
             } finally {
                 bitmap.recycle();
